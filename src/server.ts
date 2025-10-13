fcac82d946e5ab86fe759a7ccd4a252facd0faa7
Karabo Mogano <karabomogano27@gmail.com>
2025-10-14 00:13:20 +0200

    implemented backend login logic

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import loginRoute from './backend/routes/loginRoute';
 
 const app = express()
 const PORT = 3000
@@ -7,6 +8,8 @@ const PORT = 3000
 app.use(cors())
 
 app.use(express.json())
+
+app.use('/api/login', loginRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);       
