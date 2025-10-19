6a032f88ee9a49825f9f6cba0d4f61b643fdd05b
unknown <mashajan1991@gmail.com>
2025-10-19 15:24:04 +0200

    resolved merge conflict on rooms section feature and  hotel amenities feature

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,30 @@ import Navbar from './Components/Navbar/Navbar';
 import Hero from './Pages/Hero';
 import { RoomsSection } from './Components/RoomsSection/RoomsSection';
 import HotelAmenities from './Components/HotelAmenities/HotelAmenities';
+import LatestNewsEvents from './Components/NewsEvents/LatestNewsEvents';
+import BlogNewYearsGala from './Components/NewsEvents/Blog/BlogNewYearsGala';
+import BlogWinePairing from './Components/NewsEvents/Blog/BlogWinePairing';
+import BlogHiddenGems from './Components/NewsEvents/Blog/BlogHiddenGems';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route
           path="/"
           element={
             <>
-              <Navbar />
               <Hero />
               <RoomsSection />
               <HotelAmenities />
+              <LatestNewsEvents />
             </>
           }
         />
+        <Route path="/blog/new-years-eve-gala" element={<BlogNewYearsGala />} />
+        <Route path="/blog/wine-pairing-masterclass" element={<BlogWinePairing />} />
+        <Route path="/blog/hidden-gems-guide" element={<BlogHiddenGems />} />
       </Routes>
     </Router>
   );
